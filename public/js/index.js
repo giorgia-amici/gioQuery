791880afdf4ea0fe8e4eb780e440f438b136362a
Giorgia <giorgia1london@gmail.com>
2015-01-15 20:23:09 +0000

    first tests passing

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,28 @@
-function helloWorld(){
-    return 'helloWorld';
-}
+
+   var MyLibrary = function (selector) {
+
+    this.elements = [];
+
+    // if (selector[0] === "#")
+    //   elements = [document.getElementById(selector.slice(1))];
+    
+    // if (selector[0] === ".") 
+    //   elements = document.getElementsByClassName(selector.slice(1))
+ 
+    // if (selector[0] !== "#" && selector[0] !== ".")
+    //   elements = document.getElementsByTagName(selector)
+    
+    // var toArray = function(likeArrayObject){
+    //     return Array.prototype.slice.call(likeArrayObject)
+    // }
+   
+    // return toArray(elements).forEach(function(ele){
+    //   console.log(ele)
+    // })
+    // return this.elements;
+  };
+
+ MyLibrary.prototype.isId = function(selector) {
+    // code goes here
+    return true
+  }
--- public/js/spec/indexSpec.js
+++ public/js/spec/indexSpec.js
@@ -1,5 +1,27 @@
-describe("Hello world", function() {
-    it("says hello in index_spec", function() {
-        expect(helloWorld()).toEqual("helloWorld");
-    });
+describe('my own jQuery library', function(){
+
+	var gioQuery;
+
+	beforeEach(function() {
+	  gioQuery = new MyLibrary
+	});
+
+	it('is loaded', function(){
+		expect(gioQuery).toBeDefined();
+	});
+
+	it('returns an array', function(){
+		expect(gioQuery.elements).toEqual([]);
+	});
+
+	it('has a method isId', function(){
+		// console.log(gioQuery, "here I am")
+		expect(gioQuery.isId()).toBeTruthy();
+		// expect(gioQuery.isId).toHaveBeenCalled;
+		// expect(gioQuery.isId).toBe(true);
+	});
+
+	// it('should recognise if a selector is an id', function(){
+		
+	// });	
 });
