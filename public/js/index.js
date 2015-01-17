421cc97dc971412cf4aa0f7666b2ab60466a9d7a
Giorgia <giorgia1london@gmail.com>
2015-01-17 13:30:33 +0000

    omni function

--- public/js/index.js
+++ public/js/index.js
@@ -4,21 +4,26 @@ var MyLibrary = function(){
 
  MyLibrary.prototype.isTag = function(selector) {
     this.elements = document.getElementsByTagName(selector);
-    return this.elements;
+    return true;
 };
 
 MyLibrary.prototype.isClass = function(selector){
     this.elements = document.getElementsByClassName(selector);
-    return this.elements;
+    return true;
 };
 
 MyLibrary.prototype.isId = function(selector){
     this.elements = [document.getElementById(selector)];
-    return this.elements;
+    return true;
 };
 
 // The following will not save the added elements, because the .add() 
 // method creates a new set and leaves the original set in pdiv unchanged
+
+MyLibrary.prototype.fetchElementFromDOM = function(selector){
+
+};
+
 
 MyLibrary.prototype.addMe = function(){
 
@@ -28,7 +33,7 @@ MyLibrary.prototype.addMe = function(){
 
 
 var gio = new MyLibrary();
-console.log(gio, 'gio');
+// console.log(gio, 'gio');
 // console.log(Array.prototype.slice.call(gio.isTag('div')));
 // console.log(Array.prototype.slice.call(gio.isClass('some_class')));
 // console.log(Array.prototype.slice.call(gio.isId('some_id')));
