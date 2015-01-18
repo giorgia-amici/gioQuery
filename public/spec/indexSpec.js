324a0831455cddfec45db4042a2ec4d041d7bb2e
Giorgia <giorgia1london@gmail.com>
2015-01-18 16:59:02 +0000

    tests passing

--- public/spec/indexSpec.js
+++ public/spec/indexSpec.js
@@ -12,19 +12,20 @@ describe("a library for DOM manipulation", function() {
 		});
 
     it("knows when a selector is a class", function() {
-        expect(gioQuery.isClass('.Class')).toEqual(true);
+        expect(gioQuery.isClass('.class')).toEqual(true);
     });
 
     it("knows when a selector is an id", function() {
-        expect(gioQuery.isId('#Id')).toEqual(true);
+        expect(gioQuery.isId('#id')).toEqual(true);
     });
 
     it("knows when a selector is a tag", function() {
-        expect(gioQuery.isTag('Tag')).toEqual(true);
+        expect(gioQuery.isTag('tag')).toEqual(true);
     });
 
     it("returns elements by class name", function(){
-
+    	  elements = gioQuery.fetchElementFromDOM('img')[0]
+        expect(elements.outerHTML).toEqual("<img id=\"some_other_id\" class=\"some_class some_other_class\">")
     });
 
     it("returns element by id name", function(){
@@ -35,7 +36,7 @@ describe("a library for DOM manipulation", function() {
 
     });
 
-//         expect(element.outerHTML).toEqual("<img id=\"some_other_id\" class=\"some_class some_other_class\">")
+//         expect(elements.outerHTML).toEqual("<img id=\"some_other_id\" class=\"some_class some_other_class\">")
 
 
 });
