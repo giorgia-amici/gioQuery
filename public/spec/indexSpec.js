3dee87dd5c6c7ac7cee63d2b7e7a5dfdfc7396ef
Giorgia <giorgia1london@gmail.com>
2015-01-18 15:18:43 +0000

    new tests

--- public/spec/indexSpec.js
+++ public/spec/indexSpec.js
@@ -1,31 +1,37 @@
-describe('my own jQuery library', function(){
+describe("a library for DOM manipulation", function() {
+    var gioQuery;
+    var elements;
 
-	var gioQuery;
+    beforeEach(function() {
+        gioQuery = new MyLibrary();
+    });
 
-	beforeEach(function() {
-	  gioQuery = new MyLibrary();
-	});
+		it('is loaded', function(){
+			console.log(gioQuery);
+			expect(gioQuery).toBeDefined();
+		});
 
-	it('is loaded', function(){
-		console.log(gioQuery);
-		expect(gioQuery).toBeDefined();
-	});
-
-	it('returns an array', function(){
-		expect(gioQuery.isClass('.some_class')).toBeTruthy();
-	});
-
-	// it('has a method isId', function(){
-	// 	// expect(gioQuery.isId()).toBeTruthy();
-	// });
-
-	// it('should recognise if a selector is an id', function(){
-	// 	// gioQuery.isId('#div');
-	// });	
-
-
-
-
-
+    it("knows when a selector is a class", function() {
+        expect(gioQuery.isClass('.Class')).toEqual(true);
+    });
+    it("knows when a selector is an id", function() {
+        expect(gioQuery.isId('#Id')).toEqual(true);
+    });
+    it("knows when a selector is a tag", function() {
+        expect(gioQuery.isTag('Tag')).toEqual(true);
+    });
+//     it("returns elements by Tag Name", function() {
+//         element = $.getByTag('img')[0]
+//         expect(element.outerHTML).toEqual("<img id=\"some_other_id\" class=\"some_class some_other_class\">")
+//     })
+//     it("returns elements by class name", function() {
+//         element = $.getByClass('#unique')[0]
+//         expect(element.outerHTML).toEqual("<div class=\"unique\"></div>") 
+//     })
+//     it("returns elements by id", function() {
+//         element = $.getById('#some_id')[0]
+//         expect(element.outerHTML).toEqual("<div id=\"some_id\" class=\"some_class some_other_class\"></div>") 
+//     })
 
 });
+
