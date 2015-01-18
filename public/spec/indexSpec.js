b1198b97689d0360de752d7234e752a2b6605207
Giorgia <giorgia1london@gmail.com>
2015-01-18 15:21:25 +0000

    outer html

--- public/spec/indexSpec.js
+++ public/spec/indexSpec.js
@@ -14,24 +14,29 @@ describe("a library for DOM manipulation", function() {
     it("knows when a selector is a class", function() {
         expect(gioQuery.isClass('.Class')).toEqual(true);
     });
+
     it("knows when a selector is an id", function() {
         expect(gioQuery.isId('#Id')).toEqual(true);
     });
+
     it("knows when a selector is a tag", function() {
         expect(gioQuery.isTag('Tag')).toEqual(true);
     });
-//     it("returns elements by Tag Name", function() {
-//         element = $.getByTag('img')[0]
+
+    it("returns elements by class name", function(){
+
+    });
+
+    it("returns element by id name", function(){
+
+    });
+
+    it("returns elements by tag name", function(){
+
+    });
+
 //         expect(element.outerHTML).toEqual("<img id=\"some_other_id\" class=\"some_class some_other_class\">")
-//     })
-//     it("returns elements by class name", function() {
-//         element = $.getByClass('#unique')[0]
-//         expect(element.outerHTML).toEqual("<div class=\"unique\"></div>") 
-//     })
-//     it("returns elements by id", function() {
-//         element = $.getById('#some_id')[0]
-//         expect(element.outerHTML).toEqual("<div id=\"some_id\" class=\"some_class some_other_class\"></div>") 
-//     })
+
 
 });
 
